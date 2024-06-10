310517f62fde650613ee39e1e129d9e39642e502
JuanCruzCuello <pechicuello3@gmail.com>
2024-06-10 12:50:44 -0300

    cambios

--- app/App.py
+++ app/App.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+
 from models.models import db
 from models.models import Paquete, Repartidor, Sucursal, Transporte
 
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-    
+     
--- app/models/models.py
+++ app/models/models.py
@@ -1,68 +1,45 @@
-from flask_sqlalchemy import SQLAlchemy, ForeignKey
-from __main__ import App
+from flask_sqlalchemy import SQLAlchemy 
+from __main__ import app
 from sqlalchemy.orm import relationship
-
-db = SQLAlchemy(App)
-
-
-class Paquete(db.Model):
-    __tablename__ = 'Paquetes'
-    __idpac=db.Column(db.Integer, primary_key=True)
-    __numenvio=db.Column(db.Integer)
-    __peso=db.Column(db.Float)
-    __nombredestino=db.Column(db.String(50))
-    __direcciondestino=db.Column(db.String(50))
-    __entregado=db.Column(db.Boolean)
-    __observaciones=db.Column(db.String(50))
-    __idsucursal=db.Column(db.Integer,ForeignKey('sucursales.idsucursal'))
-    sucursal = relationship("Sucursal", backref="Paquetes")
-    __idtransporte=db.Column(db.Integer,ForeignKey('transportes.idtransporte'))
-    transporte = relationship("Transporte", backref="Paquetes")
-    __idrepartidor=db.Column(db.Integer,ForeignKey('repartidores.idrepartidor'))
-    repartidor = relationship("Repartidor", backref="Paquetes")
-    def __repr__(self):
-        return  "<User(ID='%d', Numero de envio='%d', Peso='%f', Nombre de destino='%s', Direccion de destino='%s', Entregado='%s', Observaciones='%s', ID de sucursal='%d', ID de transporte='%d', ID de repartidor='%d')>" % (self.__idpac, self.__numenvio, self.__peso, self.__nombredestino, self.__direcciondestino, self.__entregado, self.__observaciones, self.__idsucursal, self.__idtransporte, self.__idrepartidor)
-
-
-class Repartidor(db.Model):
-    __tablename__ = 'Repartidores'
-    __idrepartidor=db.Column(db.Integer, primary_key=True)
-    __nombre=db.Column(db.String(50))
-    __dni=db.Column(db.String(50))
-    __idsucursal=db.Column(db.Integer,ForeignKey('sucursales.idsucursal'))
-    sucursal = relationship("Sucursal", backref="Repartidores")
-    
-    def __repr__(self) -> str:
-        return "<Repartidor(ID='%d', Nombre='%s', DNI='%s', ID de sucursal='%d')>" % (self.__idrepartidor, self.__nombre, self.__dni, self.__idsucursal)
-
+db = SQLAlchemy(app)
 
 class Sucursal(db.Model):
-    __tablename__ = 'sucursales'
-    __idsucursal=db.Column(db.Integer, primary_key=True)
-    __nombre=db.Column(db.String(50))
-    __provincia=db.Column(db.String(50))
-    __direccion=db.Column(db.String(50))
-    __localidad=db.Column(db.String(50))
-    
-    def __repr__(self):
-        return "<Sucursal(ID='%d', Nombre='%s', Provincia='%s', Direccion='%s', Localidad='%s')>" % (self.__idsucursal, self.__nombre, self.__provincia, self.__direccion, self.__localidad)
+    __tablename__ = 'sucursal'
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.Integer, nullable=False)
+    provincia = db.Column(db.String(100), nullable=False)
+    localidad = db.Column(db.String(100), nullable=False)
+    direccion = db.Column(db.String(100), nullable=False)
+    repartidores = db.relationship('Repartidor', backref='sucursal', lazy=True)
+    paquetes = db.relationship('Paquete', backref='sucursal', lazy=True)
+    transportes = db.relationship('Transporte', backref='sucursal', lazy=True)
 
+class Repartidor(db.Model):
+    __tablename__ = 'repartidor'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    dni = db.Column(db.Integer, nullable=False)
+    id_sucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id'), nullable=False)
+    paquetes = db.relationship('Paquete', backref='repartidor', lazy=True)
 
+class Paquete(db.Model):
+    __tablename__ = 'paquete'
+    id = db.Column(db.Integer, primary_key=True , autoincrement=True)
+    numeroEnvio = db.Column(db.Integer, nullable=False)
+    peso = db.Column(db.Float, nullable=False)
+    nomdestinatario = db.Column(db.String(100), nullable=False)
+    dirdestinatario = db.Column(db.String(100), nullable=False)
+    entregado = db.Column(db.Boolean, nullable=False)
+    observaciones = db.Column(db.String(100), nullable=False)
+    idsucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id'), nullable=False)
+    idrepartidor = db.Column(db.Integer, db.ForeignKey('repartidor.id'), nullable=False)
+    idtransporte = db.Column(db.Integer, db.ForeignKey('transporte.id'), nullable=False)
 
 class Transporte(db.Model):
-    __tablename__ = 'transportes'
-    __idtransporte=db.Column(db.Integer, primary_key=True)
-    __numtrans=db.Column(db.Integer)
-    __fechahorasalida=db.Column(db.String(50))
-    __fechahorallegada=db.Column(db.String(50))
-    __idsucursal=db.Column(db.Integer)
-    __idsucursal=db.Column(db.Integer,ForeignKey('sucursales.idsucursal'))
-    sucursal = relationship("Sucursal", backref="Repartidores")
-
-    
-    def __repr__(self):
-        return "<Transporte(ID='%d', Numero de transporte='%d', Fecha de salida='%s', Fecha de llegada='%s', ID de sucursal='%d')>" % (self.__idtransporte, self.__numtrans, self.__fechahorasalida, self.__fechahorallegada, self.__idsucursal)
-
-
-
-
+    __tablename__ = 'transporte'
+    id = db.Column(db.Integer, primary_key=True)
+    numeroTransporte = db.Column(db.Integer, nullable=False)
+    fechahorasalida = db.Column(db.DateTime, nullable=False)
+    fechahorallegada = db.Column(db.DateTime, nullable=False)
+    idsucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id'), nullable=False)
+    paquetes = db.relationship('Paquete', backref='transporte', lazy=True)  # Relación con Paquete
