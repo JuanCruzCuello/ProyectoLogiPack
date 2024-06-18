5edffbf75aef9c4f3045c7398a43dca186a9e327
JuanCruzCuello <pechicuello3@gmail.com>
2024-06-18 19:26:05 -0300

    messi

--- app/App.py
+++ app/App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -12,6 +12,38 @@ from models.models import Paquete, Repartidor, Sucursal, Transporte
 def index():
     return render_template('template/inicio.html')
 
+
+
+
+
+
+@app.route('/Repartidor', methods=['GET', 'POST'])
+def repartidor():
+    if request.method == 'GET':
+        return render_template('template/repartidor.html')
+    elif request.method == 'POST':
+      return render_template('template/repartidor.html')
+  
+  
+  
+  
+
+@app.route('/Despachante')
+def despachante():
+     sucursales = Sucursal.query.order_by(Sucursal.numero).all()
+     return render_template('template/Despachante.html', sucursales = sucursales)
+   
+
+@app.route('/acciones_sucursal/<int:numero>')
+def acciones_sucursal(numero):
+    # Obtener la sucursal desde la base de datos por su número (ID)
+    sucursal = Sucursal.query.get(numero)
+
+    if not sucursal:
+        return "Sucursal no encontrada", 404
+
+    # Renderizar la plantilla de acciones para la sucursal seleccionada
+    return render_template('acciones_sucursal.html', sucursal=sucursal)
 
 if __name__ == "__main__":
     with app.app_context():
