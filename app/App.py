565c569d341befd1fce8ef2444c641db5f515fc7
JuanCruzCuello <pechicuello3@gmail.com>
2024-06-19 19:51:53 -0300

    cambios generales

--- app/App.py
+++ app/App.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
-
-
+import os 
 app = Flask(__name__)
+app.secret_key = os.urandom(24) 
 app.config.from_pyfile('config.py')
 
 
@@ -34,39 +34,39 @@ def login_Repartidor():
 
     return render_template('template/login_Repartidor.html')
     
-     
-  
-  
-  
-  
 
 @app.route('/Despachante', methods=['GET', 'POST'])
 def despachante():
-    global sucursaldespachante
-    if request.method == 'POST':
-        sucursaldespachante=request.form.get('sucursal.id')
-        return redirect('template/funcionalidadesucu.html')
-    else:
-        sucursales=Sucursal.query.order_by(Sucursal.numero).all()
+        sucursales=Sucursal.query.order_by(Sucursal.id).all()
         return render_template('template/Despachante.html', sucursales = sucursales)
         
     
     
     
     
-   
-     
-   
 
-@app.route('/funcionalidadesucu')
-def funcionalidadesucu(sucursales):
-    sucursales = Sucursal.query.get_or_404(Sucursal.numero)
-    return render_template('template/funcionalidadesucu.html', sucursales = sucursales)
+@app.route('/funcionalidadesucu', methods=['GET', 'POST'])
+def funcionalidadesucu():
+    sucursal_id= request.form.get('sucursal.id')
+    session['sucursal_id']= sucursal_id
+    return render_template('template/funcionalidadesucu.html')
 
+@app.route('/registrar_recepcion')
+def registrar_recepcion():
+    return render_template('template/registrar_recepcion.html')
 
+@app.route('/registrar_salida_trans')
+def registrar_salida_trans():
+    return render_template('template/registrar_salida_trans.html')
 
+@app.route('/registrar_llegada_trans')
+def Registrar_llegada_trans():
+    return render_template('template/registrar_llegada_trans.html')
+
+@app.route('/asignar_paquete')
+def Asignar_paquete():
+    return render_template('template/asignar_paquete.html')
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-     
